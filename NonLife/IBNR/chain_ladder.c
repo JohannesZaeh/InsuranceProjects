4db80445db18338601f353d22622a4bee3444018
JohannesZaeh <johanneszaeh@outlook.com>
2024-06-29 20:40:56 +0200

    improved script

--- NonLife/IBNR/chain_ladder.c
+++ NonLife/IBNR/chain_ladder.c
@@ -62,6 +62,8 @@ void fillMissingClaims(int rows, int cols, double claims[rows][cols], double cum
         for (int j = 0; j < cols; j++) {
             if (claims[i][j] == -1) {
                 claims[i][j] = cumulative_claims[i][j] - cumulative_claims[i][j-1];
+
+                
             }
         }
     }
@@ -90,8 +92,8 @@ void writeClaimsWithReservesToFile(int rows, int cols, double claims[rows][cols]
         fprintf(file, "\t");
 
         for (int j = 0; j < cols; j++) {
-            if (i + j > cols - 1) {
-                fprintf(file, "[%.2f]", claims[i][j]);
+            if (i + j > rows - 1) {
+                fprintf(file, "*%.2f", claims[i][j]);
             } else {
                 fprintf(file, "%.2f", claims[i][j]);
             }
@@ -99,7 +101,9 @@ void writeClaimsWithReservesToFile(int rows, int cols, double claims[rows][cols]
                 fprintf(file, "\t");
             }
         }
-        fprintf(file, "\n");
+        if (i < rows - 1){
+            fprintf(file, "\n");
+        }
     }
     fclose(file);
 }
@@ -144,8 +148,24 @@ int main() {
                 claims[i][j] = -1;
             }
         }
-        //move file pointer by one to skip the first column
-        fseek(file, 3, SEEK_CUR);
+        //move file pointer to skip the first column
+        if (i < 10-1) {
+            fseek(file, 3, SEEK_CUR);
+        } else if (i >= 10-1 && i < 100-1) {
+            fseek(file, 4, SEEK_CUR);
+        } else if (i >= 100-1 && i < 1000-1) {
+            fseek(file, 5, SEEK_CUR);
+        } else if (i >= 1000-1 && i < 10000-1) {
+            fseek(file, 6, SEEK_CUR);
+        } else if (i >= 10000-1 && i < 100000-1) {
+            fseek(file, 7, SEEK_CUR);
+        } else if (i >= 100000-1 && i < 1000000-1) {
+            fseek(file, 8, SEEK_CUR);
+        } else {
+            printf("Too many input rows\n");
+            return 0;
+        }
+        
     }
     fclose(file);
     
