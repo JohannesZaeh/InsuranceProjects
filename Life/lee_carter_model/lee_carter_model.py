ec858da615025c8225269637a150f2ef3c8e9bd6
JohannesZaeh <johanneszaeh@outlook.com>
2024-07-06 20:02:02 +0200

    Improved accesibility

--- Life/lee_carter_model/lee_carter_model.py
+++ Life/lee_carter_model/lee_carter_model.py
@@ -1,128 +1,178 @@
 import scipy.io
-import matplotlib.pyplot as plt
+import scipy.stats as stats
 import math
 import numpy as np
 
 from utils import load_data
+from utils import trim_qxt
 
 
+def lee_carter_model(data, timespan, confidence_level, *args):
+    '''
+    intput: path to mortality data (.txt file, seperated by tabs, first column contains years),time horizon, confidence level,
+            if required you can trim data here to e.p. only look at certain years/ages from the input. In that case,
+            input as *args: start_date,end_date,start_age,end_age,start_age_data
+    output: prediction intervals for each year of the timespan
+    '''
+
+    print("Starting Lee-Carter-Model")
+
+    qxt_dict = load_data(data)
+
+    if len(args) == 0:
+        qxt = trim_qxt(qxt_dict)
+    elif len(args) == 5:
+        qxt = trim_qxt(qxt_dict, args[0], args[1], args[2], args[3], args[4])
+    else:
+        print("invalid number of arguments.")
+        return 1
+
+    print("Fitting model...")
+
+    [A, alpha, beta, kappa] = estimate_parameters(qxt)
+
+    [log_m_model, goodness_of_fit] = calculate_model(A, alpha, beta, kappa)
+
+    print(f"Goodness of fit: {goodness_of_fit}")
+
+    prediction_intervals = project_mortality(log_m_model, beta, kappa, timespan, confidence_level)
+
+    return prediction_intervals
 
 
-# Load the qxt from a .mat file
-mat_file = scipy.io.loadmat(f'{working_dir}/Export 2.mat')
-#Extract data from .loadmat dictionary
-qxt = mat_file["export2"]
-#Trim data to only contain years 1980-2006
-qxt=qxt[130:]
+def estimate_parameters(qxt):
+    '''
+    Input: death probabilities as 2-dimensional list (here the format of the input is essential, rows are years and columns are ages)
+    Output: The matrix A, which is important for later calculations.
+            The Parameters as lists, alpha (one alpha for each age), beta (one beta for each age), kappa (one kappa for each observation year)
+    '''
+    qxt = np.array(qxt)
+    qxt = np.transpose(qxt)
+    # Create the matrix pxt; pxt = 1-qxt
+    pxt = 1 - qxt
+    # Calculate μx(t)
+    m = -1 * np.log(pxt)
+    # A = log(m)
+    A = np.log(m)
+    # ax = mean(Ax)
+    alpha = np.mean(A, axis=1)
+    # Calculate the matrix A-alpha, it's not so straightforward because it is along the rows. A is a matrix with x rows and alpha is a list of length x.
+    A_minus_alpha = A - alpha[:, np.newaxis]
+    # Singular Value Decomposition.
+    U, D, Vt = np.linalg.svd(A_minus_alpha, full_matrices=False)
+    # Transpose Vt to get V
+    V = np.transpose(Vt)
+    # Calculate c such that the betas sum up to one
+    c = 1 / np.sum(D[0] * U[:, 0])
+    # Calculate kappa
+    kappa = (1 / c) * V[:, 0]
+    # Calculate beta
+    beta = c * D[0] * U[:, 0]
 
-#Trim data to only contain ages 50 to 90
-qxt=[ages[50:91] for ages in qxt]
+    print(f"Sum Kappa: {np.sum(kappa)}")
+    print(f"Sum Beta: {np.sum(beta)}")
 
-
-'''
-(a)
-'''
-
-
-
-#To be consistent with the lecture, i transpose the data so the rows are ages and the columns are years.
-qxt = np.transpose(qxt)
-
-#Create Numpy Matrix to make it more convenient to work with.
-qxt = np.array(qxt)
-
-#Create the matrix pxt; pxt = 1-qxt
-pxt = 1-qxt
-
-#Calculate μx(t). Because we transposed the qxt, this is actually a 41x27 matrix.
-m = -1*np.log(pxt)
-
-#A = log(m)
-A = np.log(m)
-
-#ax = mean(Ax)
-ax = np.mean(A, axis = 1)
-
-#Calculate the matrix A-ax, it's not so straightforward because it is along the rows
-A_minus_ax = A - ax[:, np.newaxis]
-
-#Singular Value Decomposition.
-U,D,Vt = np.linalg.svd(A_minus_ax, full_matrices=False)
-
-#Transpose Vt to get V
-V = np.transpose(Vt)
-#Calculate c such that the betas sum up to one
-c = 1/np.sum(D[0]*U[:,0])
-
-#Calculate kappa
-kappa = (1/c)*V[:,0]
-
-#Calculate beta
-beta = c*D[0]*U[:,0]
-
-print("Test if the vecors sum up correctly:")
-print(f"Sum Kappa: {np.sum(kappa)}")
-print(f"Sum Beta: {np.sum(beta)}")
+    return [A, alpha, beta, kappa]
 
 
-'''
-(c)
-'''
-#estimate sigmasquaredepsilon
-sum = 0
-for m in range(41):
-    for n in range(27):
-        sum += (A[m,n]-ax[m]-beta[m]*kappa[n])**2
-sigmasquaredepsilon = sum/(41*27)
+def sigmasquaredepsilon(A, alpha, beta, kappa):
+    '''
+    estimate sigmasquaredepsilon
+    '''
+
+    years = len(kappa)
+    ages = len(alpha)
+
+    sum = 0
+    for m in range(ages):
+        for n in range(years):
+            sum += (A[m, n] - alpha[m] - beta[m] * kappa[n]) ** 2
+
+    sigmasquaredepsilon = sum / (ages * years)
+
+    return sigmasquaredepsilon
 
 
-print(f"Sigmasquaredepsilon: {sigmasquaredepsilon}")
+def calculate_model(A, alpha, beta, kappa):
+    '''
+    calculate the log_m_model and
+    evaluate goodness of fit of the model to the historic data.
+    output: [log_m_model,goodness of fit]
+    '''
+    years = len(kappa)
+    ages = len(alpha)
+    # First calculate the model μ. To do that i broadcast the vectors to 41x27 matrices first.
+    alpha = np.tile(alpha, (years, 1)).T
+    beta = np.tile(beta, (years, 1)).T
+    kappa = np.tile(kappa, (ages, 1))
 
-'''
-(d)
-'''
-#First calculate the model μ. To do that i broadcast the vectors to 41x27 matrices first.
-ax = np.tile(ax, (27, 1)).T
-beta = np.tile(beta, (27, 1)).T
-kappa = np.tile(kappa, (41, 1))
+    # Now calculate log(μ)
+    log_m_model = alpha + beta * kappa
 
-#Now calculate log(μ)
-log_m_model = ax+beta*kappa
+    # Now calculate the Percentage of Variance explained by the model
+    sum_model = 0
+    sum_data = 0
+    for m in range(len(alpha)):
+        for n in range(len(kappa)):
+            sum_model += (log_m_model[m, n] - alpha[m, n]) ** 2
+            sum_data += (A[m, n] - alpha[m, n]) ** 2
 
-#Now calculate the Percentage of Variance explained by the model
-sum_model = 0
-sum_data = 0
-for m in range(41):
-    for n in range(27):
-        sum_model += (log_m_model[m,n]-ax[m,n])**2
-        sum_data += (A[m,n]-ax[m,n])**2
+    goodness_of_fit = sum_model / sum_data
 
-goodness_of_fit = sum_model/sum_data
+    return [log_m_model, goodness_of_fit]
 
-print(f"Goodness of fit: {goodness_of_fit}")
 
 '''
 (e)
 '''
-#estimator C
-C = (kappa[0,26]-kappa[0,0])/26
-print(f"Estimator C: {C}")
-
-#estimator sigmasquaredkappa
-sum = 0
-for n in range(1,27):
-    sum += ((kappa[0,n]-kappa[0,n-1])-C)**2
-sigmasquaredkappa = sum/26
-print(f"Estimator sigmasquaredkappa: {sigmasquaredkappa}")
-
-'''
-(f)
-'''
-#For 2050, k=44. Compute with the formula:
-prediction_interval = [math.exp(log_m_model[30,26]+44*beta[30,26]*C-1.96*beta[30,26]*(sigmasquaredkappa**0.5)*44**0.5),math.exp(log_m_model[30,26]+44*beta[30,26]*C+1.96*beta[30,26]*(sigmasquaredkappa**0.5)*44**0.5)]
-print(f"Prediction Interval for μ for age 80 in 2050: {prediction_interval}")
 
 
-#Prediction interval for survival probabilities
-p_prediction_interval = [math.exp(-prediction_interval[1]),math.exp(-prediction_interval[0])]
-print(f"Prediction Interval for p for age 80 in 2050: {p_prediction_interval}")
+def project_mortality(log_m_model, beta, kappa, timespan, confidence_level):
+    '''
+    input:
+    output: a list of prediction intervals for each year of the timespan
+    '''
+
+    '''
+    first compute some important values
+    '''
+    ages = len(beta)
+    years = len(kappa)
+
+    # estimator C
+    C = (kappa[-1] - kappa[0]) / (years - 1)
+
+    # estimator sigmasquaredkappa
+    sum = 0
+    for n in range(1, years):
+        sum += ((kappa[n] - kappa[n - 1]) - C) ** 2
+    sigmasquaredkappa = sum / (years - 1)
+
+    '''
+    now compute the prediction intervals
+    '''
+    prediction_intervals = []
+
+    # Quantile of standard normal distribution
+    z = stats.norm.ppf(confidence_level + (1 - confidence_level) / 2)
+
+    for t in range(1, timespan):
+
+        prediction_intervals.append([])
+
+        for a in range(ages):
+            beta_now = beta[a]
+
+            deterministic_term = log_m_model[a, (years - 1)] + t * beta_now * C
+
+            probability_term = z * beta_now * (sigmasquaredkappa ** 0.5) * t ** 0.5
+
+            prediction_interval = [math.exp(deterministic_term - probability_term),
+                                   math.exp(deterministic_term + probability_term)]
+
+            # Transform force of mortality to survival probabilities
+            p_prediction_interval = [math.exp(-prediction_interval[1]), math.exp(-prediction_interval[0])]
+
+            prediction_intervals[t - 1].append(p_prediction_interval)
+
+    return prediction_intervals
--- Life/lee_carter_model/utils.py
+++ Life/lee_carter_model/utils.py
@@ -1,13 +1,49 @@
 def load_data(path):
-
+    '''
+    input: path to .txt file containing death probabilities.
+    output: dictionary where keys are years and entries are lists containing one-year-death-probability for each age.
+    '''
     data = {}
 
     with open(path, 'r') as file:
-
         for line in file:
-
             split = line.strip().split("\t")
 
-            data[split[0]] = split[1:]
+            data[split[0]] = [float(element) for element in split[1:]]
 
     return data
+
+
+def trim_qxt(qxt_dict, *argv):
+    '''
+    input: the qxt dictionary as returned by load_data(), the required ranges and the start age of the data. If no arguments specified, the dictionary
+           will just be converted to a list.
+    output: the trimmed qxt.
+    '''
+
+    qxt = []
+    #If the arguments are given, trim.
+    if len(argv) == 5:
+        start_date = argv[0]
+        end_date = argv[1]
+        start_age = argv[2]
+        end_age = argv[3]
+        start_age_data = argv[4]
+
+        for key in qxt_dict:
+            if start_date <= int(key) <= end_date:
+                try:
+                    qxt.append(qxt_dict[key][start_age-start_age_data:end_age + 1])
+                except:
+                    print("specified start age is smaller than start age of input")
+                    return 1
+     #Use entire data. This simple functionality should be possible without providing any arguments, therefore the use of *args.
+    elif len(argv) == 0:
+        for key in qxt_dict:
+            qxt.append(qxt_dict[key])
+
+    else:
+        print("invalid number of arguments.")
+        return 1
+
+    return qxt
